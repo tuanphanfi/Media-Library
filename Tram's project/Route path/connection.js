b86f2c820c67143bdcdce38faa55b2aa37d48cd1
Tram Nguyen <tramnguyen1601437@gmail.com>
2018-10-25 12:10:28 +0300

    new

---
+++ Tram's project/Route path/connection.js
@@ -0,0 +1,37 @@
+var express =require('express');
+var app = express();
+
+// var data=[];
+
+ app.set('view engine','pug');
+ app.use(express.static(__dirname + '/public'));
+
+// var MongoClient = require('mongodb').MongoClient;
+
+// MongoClient.connect("mongodb://localhost:27017/",{ useNewUrlParser: true }, function(err, db) {
+//   if(!err) {
+//     console.log("success");
+//     var dbo = db.db("mongo_test");
+//     dbo.collection("book").find({}).toArray(function(er, result) {
+//         if (er) throw er;
+//         res.render('index',{'books':result})
+//         db.close();
+//     });
+// }~
+// });
+
+app.get('/books', function(req, res){
+    res.render('books');
+});
+app.get('/contact', function(req, res){
+    res.render('contact');
+});
+app.get('/about', function(req, res){
+    res.render('about');
+});
+app.get('/', function(req, res){
+    res.render('home');
+});
+app.listen(5000, function(req,res){
+    console.log('Running');
+});
--- Tram's project/testConnectionMongodb/render.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-function onRequest(request,response){
-        response.writeHead(200,{'Content-Type':'text/plain'});
-        response.write('index.html');
-        response.end();
-}
-http.createServer(onRequest).listen(3000);
